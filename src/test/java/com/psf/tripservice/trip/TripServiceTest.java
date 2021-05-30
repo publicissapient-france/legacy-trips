87374912c479c2368bf7a9a4c930558510689e1d
Peter Onneby <ponneby@xebia.fr>
2021-05-30 23:51:06 +0200

    Refactored

--- src/test/java/com/psf/tripservice/trip/TripServiceTest.java
+++ src/test/java/com/psf/tripservice/trip/TripServiceTest.java
@@ -3,40 +3,42 @@ package com.psf.tripservice.trip;
 import com.psf.tripservice.exception.UserNotLoggedInException;
 import com.psf.tripservice.user.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TripServiceTest {
+
+    TripDAO tripDAO;
+
+    @BeforeEach
+    public void setUp() {
+        tripDAO = Mockito.mock(TripDAO.class);
+    }
+
     @Test
     void should_throw_exception_when_user_not_logged_in() {
         // Given
-        TripService tripService = new TripService() {
-            @Override
-            protected User getLoggedUser() {
-                return null;
-            }
-        };
+        TripService tripService = new TripService(tripDAO);
+
         // When
         // Then
         Assertions.assertThrows(UserNotLoggedInException.class, () -> {
-            tripService.getTripsByUser(null);
+            tripService.getTripsByUser(null, null);
         });
     }
 
     @Test
     void should_return_empty_trips_when_user_has_no_friends() {
         // Given
-        TripService tripService = new TripService() {
-            @Override
-            protected User getLoggedUser() {
-                return new User();
-            }
-        };
+        TripService tripService = new TripService(tripDAO);
+
         // When
-        List<Trip> tripsByUser = tripService.getTripsByUser(new User());
+        List<Trip> tripsByUser = tripService.getTripsByUser(new User(), new User());
         // Then
         assertThat(tripsByUser).isEmpty();
     }
@@ -44,16 +46,12 @@ class TripServiceTest {
     @Test
     void should_return_empty_trips_when_user_has_friends() {
         // Given
-        TripService tripService = new TripService() {
-            @Override
-            protected User getLoggedUser() {
-                return new User();
-            }
-        };
+        TripService tripService = new TripService(tripDAO);
+
         User user = new User();
         user.addFriend(new User());
         // When
-        List<Trip> tripsByUser = tripService.getTripsByUser(user);
+        List<Trip> tripsByUser = tripService.getTripsByUser(user, new User());
         // Then
         assertThat(tripsByUser).isEmpty();
     }
@@ -62,21 +60,14 @@ class TripServiceTest {
     void should_return_trips_when_user_is_friend_with_loggedUser() {
         // Given
         User loggedUser = new User();
-        TripService tripService = new TripService() {
-            @Override
-            protected User getLoggedUser() {
-                return loggedUser;
-            }
+        TripService tripService = new TripService(tripDAO);
 
-            @Override
-            protected List<Trip> findTripsByUser(User user) {
-                return List.of(new Trip());
-            }
-        };
         User user = new User();
         user.addFriend(loggedUser);
+        Mockito.when(tripDAO.findTripsByUser(user)).thenReturn(List.of(new Trip("Miami")));
+
         // When
-        List<Trip> tripsByUser = tripService.getTripsByUser(user);
+        List<Trip> tripsByUser = tripService.getTripsByUser(user, loggedUser);
         // Then
         assertThat(tripsByUser).isNotEmpty();
     }
